69b2d8dc08be1a7f96e500f480c03a449386dd50
zhangji <goodhorsezxj@gmail.com>
2017-04-07 15:02:03 +0800

    support options and py3web 0.3.4

--- code/index.py
+++ code/index.py
@@ -3,12 +3,20 @@ from controller import add_book, update_book, detail_book, list_book, delete_boo
 
 web.config.load('config.yml')
 
+
+def entry_wrapper(f):
+    if web.ctx.method == 'OPTIONS':
+        return ''
+    return f()
+
 app = web.application()
 
 app.add_mapping('/book/add', post=add_book)
 app.add_mapping('/book/(?<id>[0-9]+)', get=detail_book, put=update_book, delete=delete_book)
 app.add_mapping('/book/list', get=list_book)
 app.add_mapping('/apidoc', get=lambda: web.apidoc)
+
+app.add_processor(entry_wrapper)
 
 application = app.wsgifunc()
 
