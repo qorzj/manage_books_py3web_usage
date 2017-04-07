135a0f4fc603baae247263f663ad3079020024f6
zhangji <goodhorsezxj@gmail.com>
2017-04-07 15:34:23 +0800

    support py3web 0.3.5

--- code/index.py
+++ code/index.py
@@ -3,17 +3,7 @@ from controller import add_book, update_book, detail_book, list_book, delete_boo
 
 web.config.load('config.yml')
 
-
-def global_filter(f):
-    web.header('Access-Control-Allow-Origin', '*')
-    web.header('Access-Control-Allow-Headers', 'Cache-Control, Accept-Encoding, token, Origin, X-Requested-With, Content-Type, Accept, Authorization,Referer,User-Agent')
-    if web.ctx.method == 'OPTIONS':
-        return ''
-    return f()
-
-
 app = web.application()
-app.add_processor(global_filter)
 
 app.add_mapping('/book/add', post=add_book)
 app.add_mapping('/book/(?<id>[0-9]+)', get=detail_book, put=update_book, delete=delete_book)
